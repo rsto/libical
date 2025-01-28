rsto/libical

commit 7d124da53fdacc13f02ba4b3fe4597a352effe77

icaltypes.c - icalreqstattype_from_string don't allocate empty debug str
In icalreqstattype_from_string() don't allocate memory for an empty
status.debug string.

Fixes a fuzzer issue

src/libical/icaltypes.c
@@ -140,7 +140,7 @@ struct icalreqstattype icalreqstattype_from_string(const char *str)
      */
 
     p2 = strchr(p1 + 1, ';');
-    if (p2 != 0 && *p2 != 0) {
+    if (p2 != 0 && *p2 != 0 && *p2 != ';') { // skipping empty debug strings
         stat.debug = icalmemory_tmp_copy(p2 + 1);
     }
 
@@ -171,7 +171,6 @@ char *icalreqstattype_as_string_r(struct icalreqstattype stat)
     if (stat.debug != 0) {
         snprintf(temp, TMP_BUF_SIZE, "%d.%d;%s;%s", icalenum_reqstat_major(stat.code),
                  icalenum_reqstat_minor(stat.code), stat.desc, stat.debug);
-
     } else {
         snprintf(temp, TMP_BUF_SIZE, "%d.%d;%s", icalenum_reqstat_major(stat.code),
                  icalenum_reqstat_minor(stat.code), stat.desc);

src/test/CMakeLists.txt
@@ -263,6 +263,7 @@ if(LIBICAL_BUILD_TESTING_BIGFUZZ)
     69527
     69888 # cpu timeout fails beyond 60s
     42536507 # cpu timeout fails beyond 60s
+    42536390
     usban
     usban2
     usban3

test-data/fuzz42536390
@@ -1,0 +1,89 @@
+BEI:
+NOG0¶CD
+END;
+BEGIN:X-ŠX*EQUEST-STATUS;4.;N;-
+REQUEST-STATUSESTST-STATATUS;4.;N;-
+PEQUEST-ST@TUS;4.;N;-
+REQUEST-STATUS;4.;N;-
+REQUES;-
+REQUEST-STATUS;4.;N;-
+REQUEST-STATUS;4.;N;-
+REQUEST-STATUS;4.;N;-
+REQUE[T-STATUS;4.;N;-
+REQUEST-STATUS;4.;N;-
+REQUEST-STATUS;4.;N;-STATUS;9223372036854775813.;N;-
+REQUEST-STATUS;4.;N;-EÒ;VALUE=ERR*OR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VAPUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROŸ	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUOR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUEVALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;â€®VALUOR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;â€®VALUOR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUOR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+ERROR	:
+X;VALUE=EÒ;VALUE=ERROR	:
+X;VALUE=EÒ;VALUE=ERRM
+REQUEST-STATUS;4.;N;-
+REQUEST-STATUS;4.;N;-
+REQUEST-STATUS;4.-STATUS;4.;NÊÒõ­E®ªº¬³Ò¬«ATUS;4.;N;-
+REQWWWNDG2¶CD
+END;
+BE

commit 2f57a15ef951a081f60868bef9d9b64ac748f02e

icaltypes.c: fix bug omitting REQUEST-STATUS 'extdata' value
7d124da53fdacc13f02ba4b3fe4597a352effe77 introduced a bug
which resulted in the REQUEST-STATUS structured value
'extdata' part not be set in the icalreqstattype 'debug' field.

src/libical/icaltypes.c
@@ -140,7 +140,7 @@ struct icalreqstattype icalreqstattype_from_string(const char *str)
      */
 
     p2 = strchr(p1 + 1, ';');
-    if (p2 != 0 && *p2 != 0 && *p2 != ';') { // skipping empty debug strings
+    if (p2 != 0 && *(p2 + 1) != 0) { // skipping empty debug strings
         stat.debug = icalmemory_tmp_copy(p2 + 1);
     }
 

src/test/regression.c
@@ -5934,6 +5934,16 @@ static void test_icalparamiter(void)
     icalcomponent_free(comp);
 }
 
+static void test_icalreqstattype_debug(void)
+{
+    struct icalreqstattype v = icalreqstattype_from_string("2.0;Success");
+    ok("'debug' field is null", v.debug == 0);
+
+    v = icalreqstattype_from_string("3.1;Invalid property value;DTSTART:96-Apr-01");
+    ok("'debug' field contains extdata",
+            (v.debug != 0) && !strcmp(v.debug, "DTSTART:96-Apr-01"));
+}
+
 int main(int argc, char *argv[])
 {
 #if !defined(HAVE_UNISTD_H)